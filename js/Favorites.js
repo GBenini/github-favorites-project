a3d5df05014813be5bbb3fb867dc897b466e7ff5
Guilherme Benini <guigabenini@gmail.com>
2023-12-07 08:02:01 -0300

    Js att

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,15 +1,45 @@
+import { GithubUser } from "./github.js"
+
 // classe que vai conter a logica dos dados
 //como os dados serao estruturados
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
+
+        GithubUser.search('GBenini').then(user => console.log(user))
     }
 
     load() {
         this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    }
 
-        console.log(this.entries)
+    save() {
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
+
+    async add(username) {
+        try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if( userExists) {
+                throw new Error('Usuário já cadastrado' )
+            }
+
+            const user = await GithubUser.search(username)
+
+            if(user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        } catch(error) {
+            alert(error.message)
+        }
     }
 
     delete(user) {
@@ -19,8 +49,10 @@ export class Favorites {
 
         this.entries = filteredEntries
         this.update()
+        this.save()
     }
 }
+
 
 
 //classe que vai criar a visualizacao e eventos do HTML
@@ -31,7 +63,17 @@ export class FavoritesView extends Favorites {
     this.tbody = this.root.querySelector('table tbody')
 
     this.update()
+    this.onadd()
    } 
+
+   onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
+}
 
    update() {
     this.removeAllTr()
@@ -41,17 +83,18 @@ export class FavoritesView extends Favorites {
 
        row.querySelector('.user img').src = `https://github.com/${user.login}.png`
        row.querySelector('.user img').alt = `Imagem de ${user.name}`
+       row.querySelector('.user a').href = `https://github.com/${user.login}`
        row.querySelector('.user p').textContent = user.name
        row.querySelector('.user span').textContent = user.login
        row.querySelector('.repositories').textContent = user.public_repos
        row.querySelector('.followers').textContent = user.followers
 
        row.querySelector('.remove').onclick = () => {
-        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
-        if(isOK) {
-            this.delete(user)
+        const isOk = confirm('Tem certeza que deseja deletar esse Usuário?')
+        if(isOk) {
+          this.delete(user)
         }
-       }
+      }
 
        this.tbody.append(row)
     })
